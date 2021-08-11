72d93e3a6cbe33c2cfd7d9cbfa7e71fabb9ebdc7
jasonsmith9962 <jasonsmith9962@gmail.com>
2021-08-11 11:24:16 -0400

    Started Navbar and footer

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/site/Sidebar';
+import Footer from './components/site/Footer';
+import Navbar from './components/site/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Router>
       <Sidebar />
       </Router>
-      
+      <Footer />
     </div>
   );
 }
